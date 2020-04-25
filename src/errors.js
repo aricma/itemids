f89d1ebb601728d1e4431e65d94786fb5b331b73
aricma <adrian@aricma.com>
2020-04-26 00:52:30 +0200

    fix errors + write the unify error to pass the tests

--- src/errors.js
+++ src/errors.js
@@ -2,11 +2,17 @@ export const ItemIds = {
 	givenWasNoArray: "ItemIds expects one argument of type Array."
 };
 
-export const functionFactories = {
+export const utils = {
 	_with: {
 		gotFunction: "_with does not allow given value to be a function."
 	},
 	_can: {
 		gotNoFunction: "_can expects the given arg \"func\" to be of type function"
+	}
+};
+
+export const staticMethods = {
+	unify: {
+		gotNoItemIdList: "unify expects one argument of type ItemIdList"
 	}
 };
