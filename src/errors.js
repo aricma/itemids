75d667ecbd897368b10c98e1f228c7994a1e646b
aricma <adrian@aricma.com>
2020-04-26 04:25:41 +0200

    write tests for all the method errors + write the errors to pass the tests

--- src/errors.js
+++ src/errors.js
@@ -21,5 +21,29 @@ export const methods = {
 	set: {
 		isNotBoundToAnArray: "set needs to be bound to an Array",
 		gotNeitherItemIdListNorFunction: "set expects one argument of either type ItemIdList or function",
+	},
+	add: {
+		isNotBoundToAnArray: "add needs to be bound to an Array",
+		gotNeitherItemIdNorItemIdList: "add expects one argument of either type ItemId or ItemIdList"
+	},
+	remove: {
+		isNotBoundToAnArray: "remove needs to be bound to an Array",
+		gotNeitherItemIdNorItemIdList: "remove expects one argument of either type ItemId or ItemIdList"
+	},
+	toggle: {
+		isNotBoundToAnArray: "toggle needs to be bound to an Array",
+		gotNeitherItemIdNorItemIdList: "toggle expects one argument of either type ItemId or ItemIdList"
+	},
+	toggleAll: {
+		isNotBoundToAnArray: "toggleAll needs to be bound to an Array",
+		gotNoItemIdList: "toggleAll expects one argument of type ItemIdList"
+	},
+	has: {
+		isNotBoundToAnArray: "has needs to be bound to an Array",
+		gotNeitherItemIdNorItemIdList: "has expects one argument of either type ItemId or ItemIdList"
+	},
+	isEqualTo: {
+		isNotBoundToAnArray: "isEqualTo needs to be bound to an Array",
+		gotNoItemIdList: "add expects one argument of type ItemIdList"
 	}
 };
--- src/errors.test.js
+++ src/errors.test.js
@@ -57,6 +57,78 @@ describe("ERRORS", () => {
 
         });
 
+        describe("add", () => {
+
+            test("isNotBoundToAnArray", () => {
+                expect(typeof ERRORS.methods.add.isNotBoundToAnArray).toBe("string");
+            });
+
+            test("gotNeitherItemIdNorItemIdList", () => {
+                expect(typeof ERRORS.methods.add.gotNeitherItemIdNorItemIdList).toBe("string");
+            });
+
+        });
+
+        describe("remove", () => {
+
+            test("isNotBoundToAnArray", () => {
+                expect(typeof ERRORS.methods.remove.isNotBoundToAnArray).toBe("string");
+            });
+
+            test("gotNeitherItemIdNorItemIdList", () => {
+                expect(typeof ERRORS.methods.remove.gotNeitherItemIdNorItemIdList).toBe("string");
+            });
+
+        });
+
+        describe("toggle", () => {
+
+            test("isNotBoundToAnArray", () => {
+                expect(typeof ERRORS.methods.toggle.isNotBoundToAnArray).toBe("string");
+            });
+
+            test("gotNeitherItemIdNorItemIdList", () => {
+                expect(typeof ERRORS.methods.toggle.gotNeitherItemIdNorItemIdList).toBe("string");
+            });
+
+        });
+
+        describe("toggleAll", () => {
+
+            test("isNotBoundToAnArray", () => {
+                expect(typeof ERRORS.methods.toggleAll.isNotBoundToAnArray).toBe("string");
+            });
+
+            test("gotNoItemIdList", () => {
+                expect(typeof ERRORS.methods.toggleAll.gotNoItemIdList).toBe("string");
+            });
+
+        });
+
+        describe("has", () => {
+
+            test("isNotBoundToAnArray", () => {
+                expect(typeof ERRORS.methods.has.isNotBoundToAnArray).toBe("string");
+            });
+
+            test("gotNeitherItemIdNorItemIdList", () => {
+                expect(typeof ERRORS.methods.has.gotNeitherItemIdNorItemIdList).toBe("string");
+            });
+
+        });
+
+        describe("isEqualTo", () => {
+
+            test("isNotBoundToAnArray", () => {
+                expect(typeof ERRORS.methods.isEqualTo.isNotBoundToAnArray).toBe("string");
+            });
+
+            test("gotNoItemIdList", () => {
+                expect(typeof ERRORS.methods.isEqualTo.gotNoItemIdList).toBe("string");
+            });
+
+        });
+
     });
 
 });
