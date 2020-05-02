616e7b224faffd15dd6f66ed1b457705f914cdfa
aricma <adrian@aricma.com>
2020-05-02 22:57:10 +0200

    clean code

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 // babel.config.js
 module.exports = api => {
-	const isTest = api.env('test');
+	// const isTest = api.env('test');
 	// You can use isTest to determine what presets and plugins to use.
 	
 	return {
